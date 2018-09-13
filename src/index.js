605d9dde6e4fd0f59ed329ef4c1ed025451e71db
mbulldozer <mbulldozer@mail.ru>
2018-09-14 01:04:50 +0300

    love_triangle: task complete

--- src/index.js
+++ src/index.js
@@ -3,5 +3,20 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+    // implementation
+    let num_spichonee = preferences.length;
+    let spichonee_a, spichonee_b, spichonee_c;
+    let count_triangles = 0;
+    for(let i = 1; i <= num_spichonee; i++)
+    {
+        spichonee_a = preferences[i - 1];
+        if(spichonee_a == i) continue;
+        spichonee_b = preferences[spichonee_a - 1];
+        if(spichonee_b == spichonee_a) continue;
+        spichonee_c = preferences[spichonee_b - 1];
+            if(spichonee_c == i){
+              count_triangles++;
+            }
+    }
+    return count_triangles / 3;
 };
